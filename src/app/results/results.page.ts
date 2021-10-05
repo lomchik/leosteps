2644172f9003583a4f2bceea3d1e8060366b6155
Roman Melnychuk <lomchik1@gmail.com>
2021-10-05 19:04:42 +0300

    add sharing

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-results',
@@ -8,15 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class ResultsPage implements OnInit {
 
   data = localStorage;
+  shared = false;
 
-  constructor() {
-
+  constructor(private nav: NavController) {
   }
 
   ngOnInit() {
   }
 
   send() {
+    // @ts-ignore
+    window.plugins.socialsharing.share(
+      this.getStringData(),
+      'Результати тесту ' + this.data['id']
+    );
+    this.shared = true;
+  }
 
+  getStringData() {
+    return `
+Індивідуальний номер: ${this.data['id']}
+Перед тестуванням
+  Пульс: ${this.data['heartRate']}
+  Сатурація: ${this.data['saturation']}
+  Віддишка: ${this.data['breathBeforeTest']}
+  Втома: ${this.data['tensionBeforeTest']}
+Тестування:
+  Кроків: ${this.data['steps']}
+  Біль: ${this.data['testPain']}
+Після тестування:
+  Пульс: ${this.data['heartRateAfter']}
+  Сатурація: ${this.data['saturationAfter']}
+  Віддишка: ${this.data['breathAfterTest']}
+  Втома: ${this.data['tensionAfterTest']}
+    `;
+  }
+
+  begin() {
+    this.nav.navigateForward(['/']);
   }
 }
